a8130108db62576c3d6ac110ea50d55e03f0f412
Tobias Jane <vinopaldimit@gmail.com>
2019-04-28 19:07:35 -0400

    inital commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+
+	/*constructor (props) {
+		super(props)
+	}*/
+
+
+    render() {
+        return (
+        	<header>
+        		fuck
+        	</header>
+        	);
+    }
+}
+
+export default Header;
